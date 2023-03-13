dab1be872cbea036485909a4306a1348c9d7a715
Michał <michal.szewczyk.200@gmail.com>
2023-03-13 23:17:34 +0100

    Initial commit

---
+++ src/test/java/com/example/gabinetrehabilitacji/GabinetRehabilitacjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gabinetrehabilitacji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabinetRehabilitacjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
