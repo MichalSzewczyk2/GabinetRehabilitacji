95893c8e79e7dc9c9ead7cff3c75816e3690e43f
Michał <michal.szewczyk.200@gmail.com>
2023-05-08 21:14:43 +0200

    main page without content

--- src/main/java/com/clinic/pageController/mainPageController.java
+++ src/main/java/com/clinic/pageController/mainPageController.java
@@ -2,12 +2,22 @@ package com.clinic.pageController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class mainPageController {
 
   @GetMapping("/main")
-  public String getMain(){
+  public String getMain(@RequestParam(name = "page", required = false) String page) {
+
+    if(page != null){
+      if(page.equals("home")){
+        return "mainPage";
+      }
+      if(page.equals("contact")) {
+        return "contactPage";
+      }
+    }
     return "mainPage";
   }
 
